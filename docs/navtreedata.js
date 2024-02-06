b239944ec35c6c3947933162ec076462fe6618c6
jpere180 <156122419+jpere180@users.noreply.github.com>
2024-02-06 11:27:58 -0800

    doxygen update

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Gray Area", "index.html", [
+  [ "Lab 02: Gray Area", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
