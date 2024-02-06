a63f5817f15c4b3140cd668a4a2d6fc5305c2707
jpere180 <156122419+jpere180@users.noreply.github.com>
2024-02-06 09:26:45 -0800

    test

--- src/encoder_reader_test.py
+++ src/encoder_reader_test.py
@@ -20,25 +20,22 @@ from encoder_reader import Encoder
     
 # This main code is run if this file is the main program but won't run if this
 # file is imported as a module by some other main program
-if __name__ == "__main__":# set up timer 3
-    TIM3 = pyb.Timer(3, freq=2000) # Timer 3, frequency 2000Hz
-    # Define pin assignments for motor 1
-    ENA = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
-    IN1A = pyb.Pin(pyb.Pin.board.PB4)
-    IN2A = pyb.Pin(pyb.Pin.board.PB5)
-
-
-    # Create motor drivers
-    moe = MotorDriver(ENA, IN1A, IN2A, TIM3)
+if __name__ == "__main__":
+    # set up timer 4 for encoder 1
+    TIM4 = pyb.Timer(4, prescaler=1, period=100000) # Timer 3, no prescalar, frequency 100kHz
+    # set up timer 3 for encoder 2
+    TIM3 = pyb.Timer(3, prescaler=1, period=100000) # Timer 3, no prescalar, frequency 100kHz
+    # Define pin assignments for encoder 1
+    pinb6 = pyb.Pin(pyb.Pin.board.PB6)
+    pinb7 = pyb.Pin(pyb.Pin.board.PB7)
+    #Define pin assignments for encoder 2
+    pinc6 = pyb.Pin(pyb.Pin.board.PC6)
+    pinc7 = pyb.Pin(pyb.Pin.board.PC7)
+    # Create first encoder object
+    Tom = Encoder(pinb6, pinb7, TIM4)
+    Jerry = Encoder(pinc6, pinc7, TIM3)
+    
     while True:
-        moe.set_duty_cycle (100)				#Forward at 50% duty cycle
-        time.sleep(2)						#Sleeps for 2 seconds
-        moe.set_duty_cycle (-50)			#Reverse at 50% duty cycle
-        time.sleep(2)						#Sleeps for 2 seconds
-        moe.set_duty_cycle (50)			#Reverse at 50% duty cycle
-        time.sleep(2)
-        moe.set_duty_cycle (0)				#Stops the duty cycle
-        time.sleep(2)						#Sleeps for 2 seconds
-
-
+        time.sleep(0.01)
+        Tom.read()
 
